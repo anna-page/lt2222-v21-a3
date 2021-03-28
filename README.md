31f826730c77b8c044d9bcdec1dc32dafa9e48c0
Anna Page <guspagan@GU.GU.SE@eduserv.flov.gu.se>
2021-03-28 13:42:37 +0200

    added eval file

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # LT2222 V21 Assignment 3
 
-Your name:
+Your name: Anna Page 
 
 ## Part 1
+
+Function a:
+
+f = input file (data to be processed)
+l = a line of the data in the input file
+[c for c in l] = adds characters in line to mm (a list of every character, including spaces and new line characters)
+adds two start and two end tags to the list
+returns the full list of characters as well as a list of the unique characters as a tuple which is assigned to q
+
+Function b:
+
+Takes two arguments: u - the full list of characters, and p - the list of unique characters (the tuple output by a)
+
+For each element in the list of all characters (given enough space on the sides to not be out of range (hence the addition of the start and end tokens)), when the current iteration (v+2) comes to a vowel, the code retrives the index of that vowel in the list of vowels and appends it to a list named 'gt', which is later turned into a numpy array. Next, the code considers the character at the present index (two before the vowel), the next item (one before the vowel) and the two items after the vowel (u[v+3] and u[v+4]). For each of these, the code makes a numpy array the lenghth of the ordered list of the set of characters and puts a 1 at the index of the relevent character. It then concatenates these four numpy arrays (vectors) into one single 1D numpy array. This long numpy array is then appended to the list gr. Finally, the list of numpy arrays is turned into a matrix where each row represents one of the numpy arrays in gr. Similarly, 'gt' (the list of vowel indices) is converted to a 1D numpy array.
+
+Function g:
+
+This function is used by function b. It receives two arguments: a character and the list of unique characters. It creates a numpy array equal in length to the length of the unique character list. It then sets the value of that array to 1 at the index of that character in the list of unique characters.
+
+Argument k:
+
+This is an optional argument with a default value of 200. It represents the number of nodes in the hidden layers of the model. 
+
+Argument r:
+
+This is an optional argument with a default value of 100. It represeents the number of epochs in the model, which is the number of full training passes of the data that are used in the training. 
+
+Argument m:
+
+This is a required argument which constitutes the input file name. This file is loaded by a and is then processed by a and b into the input and target data which is then used to train a model.
+
+Argument h:
+
+This is a required argument and is used as the save path for the model which, once training has completed, dumps a pickle to this file path.
 
 ## Part 2
 
---
+++ eval.py
@@ -0,0 +1,113 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"]=""
+os.environ["USE_CPU"]="1"
+
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+from model import train
+import torch
+
+vowels = sorted(['y', 'é', 'ö', 'a', 'i', 'å', 'u', 'ä', 'e', 'o'])
+
+def process_test_data(test_file):
+    with open(test_file, "r") as test_data:
+        total_list = []
+        for line in test_data:
+            for character in line:
+                total_list.append(character)
+
+        total_list = ["<s>", "<s>"] + total_list + ["<e>", "<e>"]
+        return total_list
+
+def character_vector(character, char_set):
+    sub_array = np.zeros(len(char_set))
+    sub_array[char_set.index(character)] = 1
+    return sub_array
+
+def create_test_data(all_chars, char_set):
+    index_list = []
+    vectors_list = []
+    for idx in range(len(all_chars) - 4):
+
+        window_not_in_char_set = [
+            x not in char_set for x in [
+                all_chars[idx],
+                all_chars[idx + 1],
+                all_chars[idx + 3],
+                all_chars[idx + 4],
+            ]
+        ] 
+
+        if (all_chars[idx + 2] not in vowels) or any(window_not_in_char_set):
+            continue
+
+        vowel_index = vowels.index(all_chars[idx + 2])
+        index_list.append(vowel_index)
+
+        curr_vector = np.concatenate([
+            character_vector(character, char_set) for character in 
+            [
+                all_chars[idx], 
+                all_chars[idx + 1], 
+                all_chars[idx + 3], 
+                all_chars[idx + 4]
+            ]
+        ])
+        vectors_list.append(curr_vector)
+
+    return np.array(vectors_list), np.array(index_list)
+
+def predict_vowels(model, input_data):
+    predictions = model(torch.Tensor(input_data)).detach().numpy()
+    predicted_vowels = np.argmin(np.abs(predictions), axis=1)
+    return predicted_vowels
+    
+
+def accuracy(true, pred):
+    accuracy = sum(true == pred) / len(pred)
+    print(f"Model accuracy: {round(accuracy * 100, 2)}%")
+
+def write_output_data(test_file, output_file, predicted_vowels):
+    with open(test_file, "r") as f:
+        original = f.read()
+
+    original = list(original)
+
+    pred_idx = 0
+    for idx, char in enumerate(original):
+        if pred_idx < len(predicted_vowels):
+            if char in vowels:
+                original[idx] = vowels[predicted_vowels[pred_idx]]
+            pred_idx +=1 
+            # import pdb; pdb.set_trace()
+
+    with open(output_file, "w") as f:
+        f.write("".join(original))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("test_file", type=str)
+    parser.add_argument("model_pickle", type=str)
+    parser.add_argument("output_file", type=str)
+
+    args = parser.parse_args()
+
+    model = torch.load(args.model_pickle)
+    model.eval()
+
+    all_characters = process_test_data(args.test_file)
+
+    test_input, true_vowel_index = create_test_data(
+        all_characters, 
+        model.vocab
+    )
+
+    predicted_vowel_index = predict_vowels(model, test_input)
+    accuracy(true_vowel_index, predicted_vowel_index)
+
+    write_output_data(args.test_file, args.output_file, predicted_vowel_index)
+
